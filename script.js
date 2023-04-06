7274c87eacd9f6aa8bcca3daf9e6ef7fa127d9c6
Abhi Pandey <97884726+abhipandey1402@users.noreply.github.com>
2023-04-06 13:59:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // your code here
+const form = document.querySelector('#myForm');
+const url = document.querySelector('#url');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  
+  const nameInput = document.querySelector('#name');
+  const yearInput = document.querySelector('#year');
+  
+  const nameValue = nameInput.value;
+  const yearValue = yearInput.value;
+  
+  const queryString = `?name=${nameValue}&year=${yearValue}`;
+  
+  url.textContent = `https://localhost:8080/${queryString}`;
+});
